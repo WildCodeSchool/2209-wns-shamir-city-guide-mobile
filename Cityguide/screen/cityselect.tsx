77d354a1af010ba0f416cc17edf16e0558b3a4d8
ShishiFoxy <shirley.bsth@gmail.com>
2023-01-27 11:32:46 +0100

    test

--- Cityguide/screen/cityselect.tsx
+++ Cityguide/screen/cityselect.tsx
@@ -17,5 +17,5 @@ export class Point {
 
 const point1 = new Point(1, 'toto', 'tata',37.78825,-122.4324)
 const point2 = new Point(2, 'titi', 'tata',36.78825,-122.4350)
-const point3 = new Point(2, 'titi', 'tata',37.78825,-120.4350)
+const point3 = new Point(3, 'titi', 'tata',37.78825,-120.4350)
 export const points = [point1, point2, point3];
