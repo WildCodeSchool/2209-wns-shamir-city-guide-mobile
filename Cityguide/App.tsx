93a9039c94afe67e4d248a5a41d17f27aa7327db
DESKTOP-GNCP18R\Misscrazy <mehdi.mouilbeau@hotmail.fr>
2023-04-27 14:37:20 +0200

    fix

--- Cityguide/App.tsx
+++ Cityguide/App.tsx
@@ -7,9 +7,40 @@ import ConnectScreen from "./screen/connectscreen";
 import { createStackNavigator } from '@react-navigation/stack';
 import RegisterPage from "./screen/log/register";
 import LoginPage from "./screen/log/login";
+import Constants from "expo-constants";
+import { createHttpLink } from "@apollo/client/link/http/createHttpLink";
+import { ApolloClient } from "@apollo/client/core/ApolloClient";
+import { InMemoryCache } from "@apollo/client/cache/inmemory/inMemoryCache";
+import {setContext} from "@apollo/client/link/context";
+import * as SecureStore from "expo-secure-store";
 
+const { manifest } = Constants;
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
+
+
+const uri = `http://${manifest?.debuggerHost?.split(':').shift()}:5000/graphql`;
+const httpLink = createHttpLink({
+  uri,
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await SecureStore.getItemAsync("token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const appName: string = 'mobile-builder';
 
 export default function App() {
   return (
