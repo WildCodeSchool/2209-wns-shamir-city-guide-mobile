8655848eb15ae6ecf144ab47bf2341699f9529b0
DESKTOP-GNCP18R\Misscrazy <mehdi.mouilbeau@hotmail.fr>
2023-06-23 09:37:09 +0200

    Feat : Map + select city

--- Cityguide/src/screens/Globalmap.tsx
+++ Cityguide/src/screens/Globalmap.tsx
@@ -3,6 +3,8 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { GetAllCities } from '../services/city';
 import { ICity } from '../types/city';
+import MapView from 'react-native-maps';
+import RNPickerSelect from 'react-native-picker-select';
 
 
 const Globalmap: React.FC = () => {
@@ -32,6 +34,22 @@ const Globalmap: React.FC = () => {
       {allCities?.getAllCities.map((city: ICity, index: number) => (
         <View key={index}>
           <Text>{city.name}</Text>
+           <RNPickerSelect
+      onValueChange={(value: any) => console.log(value)}
+      items={allCities?.getAllCities.map((city: ICity) => ({
+        label: city.name,
+        value: city.name,
+      }))}
+    />
+           <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 50.85045,
+        longitude: 4.34878,
+        latitudeDelta: 40,
+        longitudeDelta: 40,
+      }}
+    />
         </View>
       ))}
 
